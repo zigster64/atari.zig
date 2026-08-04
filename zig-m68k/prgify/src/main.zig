c8e2c306453d8991b506db9877a5ebd9977371da
𒀳 Scribe of the Ziggurat𒅆𒂍𒉪 <steve@raffmail.net>
2026-08-04 12:15:53 +1000

    start adding prgify utility

---
+++ zig-m68k/prgify/src/main.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+const Io = std.Io;
+
+const prgify = @import("prgify");
+
+pub fn main(init: std.process.Init) !void {
+    const arena: std.mem.Allocator = init.arena.allocator();
+    const args = try init.minimal.args.toSlice(arena);
+    for (args) |arg| {
+        std.log.info("arg: {s}", .{arg});
+    }
+
+    const io = init.io;
+    _ = io; // autofix
+}

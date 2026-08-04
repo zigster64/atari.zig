63869b0661b8a86fd094351434b43f9291bedf47
𒀳 Scribe of the Ziggurat𒅆𒂍𒉪 <steve@raffmail.net>
2026-08-04 11:58:10 +1000

    missed displacing the _start offset - will do this in bash for now, then write a proper TOS Linker afterwards

--- hello/build.zig
+++ hello/build.zig
@@ -35,7 +35,7 @@ pub fn build(b: *std.Build) void {
         \\DATA_SZ=$(m68k-elf-size -A "$DIR/hello.elf" | awk '/^\.data/{print $2}')
         \\BSS_SZ=$(m68k-elf-size -A "$DIR/hello.elf"  | awk '/^\.bss/{print $2}')
         \\START_VMA=$(m68k-elf-nm "$DIR/hello.elf" | awk '$3=="_start"{print $1}')
-        \\DISP=$((16#$START_VMA - 2))
+        \\DISP=$((16#$START_VMA + 0x1c - 2))
         \\
         \\python3 -c "
         \\import struct, sys
@@ -43,7 +43,7 @@ pub fn build(b: *std.Build) void {
         \\hdr = struct.pack('>2s6Ih',
         \\    b'\\x60' + bytes([disp]),      # BRA.S to _start
         \\    $TEXT_SZ, $DATA_SZ, $BSS_SZ,   # segment sizes
-        \\    0, 0, 0, 1,                    # sym, reserved, flags, absflag
+        \\    0, 0, 0, 0,                    # sym, reserved, flags, absflag
         \\)
         \\assert len(hdr) == 28
         \\sys.stdout.buffer.write(hdr)
